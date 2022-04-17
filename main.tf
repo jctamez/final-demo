0ba6f79b4a28135222e795c2b334e283f360eafa
jctamez <101284054+jctamez@users.noreply.github.com>
2022-04-17 16:20:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ terraform {
   }
 }
 
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
 # VPC *********************************************************************************************
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
