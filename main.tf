20e0f11de06ed9716f043d193d0cca631e2f2825
jctamez <101284054+jctamez@users.noreply.github.com>
2022-04-17 16:16:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 terraform {
+	
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+	
   required_version = ">= 0.13"
+	  
   backend "s3" {
     profile        = "Final-Demo"
     region         = "us-east-1"
